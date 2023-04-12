6ff31d07ba866d3158d2fec705c1bacb95c5e580
AbitBrookish <brooksjohnson25@gmail.com>
2023-04-12 14:50:05 -0400

    assign values to variables

---
+++ StudentActivity2/script.js
@@ -0,0 +1,7 @@
+let avatar = document.querySelector('#img');
+let title = document.querySelector('#header');
+let table = document.querySelect('#table__anime');
+let column = document.querySelectorAll('.table__anime-header');
+let data = document.querySelectorAll('.table__anime-data');
+
+
