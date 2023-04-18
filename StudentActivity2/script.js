c3882b5dfe12c44f520d854df30d21190d8d9fb9
AbitBrookish <brooksjohnson25@gmail.com>
2023-04-18 05:15:06 -0400

    modify variables, create attributes

--- StudentActivity2/script.js
+++ StudentActivity2/script.js
@@ -1,7 +1,18 @@
-let avatar = document.querySelector('#img');
-let title = document.querySelector('#header');
-let table = document.querySelect('#table__anime');
-let column = document.querySelectorAll('.table__anime-header');
-let data = document.querySelectorAll('.table__anime-data');
+const avatar = document.querySelector("#img");
+const title = document.querySelector("#header");
+const table = document.querySelector("#table__anime");
+const column = document.querySelectorAll(".table__anime-header");
+const data = document.querySelectorAll(".table__anime-data");
 
 
+avatar.setAttribute("style", "width: 250px");
+title.setAttribute("style", "font-size: 40px; color: blue");
+table.setAttribute("style", "border: 1px solid black; background-color: rgba(0,0,0,0.5)");
+
+column.forEach( item => {
+  item.setAttribute("style", "padding: 16px");
+});
+
+data.forEach( item => {
+  item.setAttribute("style", "padding: 16px");
+});
